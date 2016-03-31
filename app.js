411fbbca84198740ec0d1cc9e2c400dd6b17c548
Chris Perez <chris.k.perez@gmail.com>
2016-03-30 19:23:04 -0700

    Got pretty much everything working, but apparently the chart has to show up below the images

--- app.js
+++ app.js
@@ -2,48 +2,52 @@ var imageObjectsArray = [];
 var bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, babySweep, tauntaun, unicorn, usb, waterCan, wineGlass;
 var possibleChoiceBoxes;
 var totalClicks = 0;
+var historicalIndividualClicks = [];
+var historicalIndividualDisplays = [];
+var clickPercentageArray = [];
+var labelsArray = [];
 
-function MakeImageObject(name, filePath) {
+function ImageObject(name, filePath) {
   this.name = name;
   this.filePath = filePath;
   this.timesDisplayed = 0;
   this.timesClicked = 0;
 }
 // make methods below here...
-MakeImageObject.prototype.iWasClicked = function(){
+ImageObject.prototype.iWasClicked = function(){
   this.timesClicked ++;
 };
 
-MakeImageObject.prototype.iWasDisplayed = function(){
+ImageObject.prototype.iWasDisplayed = function(){
   this.timesDisplayed ++;
 };
 
-MakeImageObject.prototype.giveMeID = function(){
+ImageObject.prototype.giveMeID = function(){
   this.myID = this.name.split(' ').join('').toLowerCase();
 };
 // make methods above here...
 
 //Data input below...
-imageObjectsArray.push(bag = new MakeImageObject('R2-D2 Luggage', 'img/bag.jpg'));
-imageObjectsArray.push(banana = new MakeImageObject('Banana Slicer', 'img/banana.jpg'));
-imageObjectsArray.push(bathroom = new MakeImageObject('iPad TP Roll', 'img/bathroom.jpg'));
-imageObjectsArray.push(boots = new MakeImageObject('Pointless Rain Boots', 'img/boots.jpg'));
-imageObjectsArray.push(breakfast = new MakeImageObject('All-in-One Breakfast Maker', 'img/breakfast.jpg'));
-imageObjectsArray.push(bubblegum = new MakeImageObject('Meatball Bubblegum', 'img/bubblegum.jpg'));
-imageObjectsArray.push(chair = new MakeImageObject('Weird Red Chair', 'img/chair.jpg'));
-imageObjectsArray.push(cthulhu = new MakeImageObject('Cthulhu Action Figure', 'img/cthulhu.jpg'));
-imageObjectsArray.push(dogDuck = new MakeImageObject('Duckbill Dog Muzzle', 'img/dog-duck.jpg'));
-imageObjectsArray.push(dragon = new MakeImageObject('Canned Dragon Meat', 'img/dragon.jpg'));
-imageObjectsArray.push(pen = new MakeImageObject('Pen Utensils', 'img/pen.jpg'));
-imageObjectsArray.push(petSweep = new MakeImageObject('Pet Sweep', 'img/pet-sweep.jpg'));
-imageObjectsArray.push(scissors = new MakeImageObject('Pizza Scissors', 'img/scissors.jpg'));
-imageObjectsArray.push(shark = new MakeImageObject('Shark Sleeping Bag', 'img/shark.jpg'));
-imageObjectsArray.push(babySweep = new MakeImageObject('Floor Sweeping Baby Clothes', 'img/sweep.png'));
-imageObjectsArray.push(tauntaun = new MakeImageObject('Tauntaun Sleeping Bag', 'img/tauntaun.jpg'));
-imageObjectsArray.push(unicorn = new MakeImageObject('Canned Unicorn Meat', 'img/unicorn.jpg'));
-imageObjectsArray.push(usb = new MakeImageObject('Dragon Tail USB', 'img/usb.gif'));
-imageObjectsArray.push(waterCan = new MakeImageObject('Pointless Watering Can', 'img/water-can.jpg'));
-imageObjectsArray.push(wineGlass = new MakeImageObject('Terrible Wine Glass', 'img/wine-glass.jpg'));
+imageObjectsArray.push(bag = new ImageObject('R2-D2 Luggage', 'img/bag.jpg'));
+imageObjectsArray.push(banana = new ImageObject('Banana Slicer', 'img/banana.jpg'));
+imageObjectsArray.push(bathroom = new ImageObject('iPad TP Roll', 'img/bathroom.jpg'));
+imageObjectsArray.push(boots = new ImageObject('Pointless Rain Boots', 'img/boots.jpg'));
+imageObjectsArray.push(breakfast = new ImageObject('All-in-One Breakfast Maker', 'img/breakfast.jpg'));
+imageObjectsArray.push(bubblegum = new ImageObject('Meatball Bubblegum', 'img/bubblegum.jpg'));
+imageObjectsArray.push(chair = new ImageObject('Weird Red Chair', 'img/chair.jpg'));
+imageObjectsArray.push(cthulhu = new ImageObject('Cthulhu Action Figure', 'img/cthulhu.jpg'));
+imageObjectsArray.push(dogDuck = new ImageObject('Duckbill Dog Muzzle', 'img/dog-duck.jpg'));
+imageObjectsArray.push(dragon = new ImageObject('Canned Dragon Meat', 'img/dragon.jpg'));
+imageObjectsArray.push(pen = new ImageObject('Pen Utensils', 'img/pen.jpg'));
+imageObjectsArray.push(petSweep = new ImageObject('Pet Sweep', 'img/pet-sweep.jpg'));
+imageObjectsArray.push(scissors = new ImageObject('Pizza Scissors', 'img/scissors.jpg'));
+imageObjectsArray.push(shark = new ImageObject('Shark Sleeping Bag', 'img/shark.jpg'));
+imageObjectsArray.push(babySweep = new ImageObject('Floor Sweeping Baby Clothes', 'img/sweep.png'));
+imageObjectsArray.push(tauntaun = new ImageObject('Tauntaun Sleeping Bag', 'img/tauntaun.jpg'));
+imageObjectsArray.push(unicorn = new ImageObject('Canned Unicorn Meat', 'img/unicorn.jpg'));
+imageObjectsArray.push(usb = new ImageObject('Dragon Tail USB', 'img/usb.gif'));
+imageObjectsArray.push(waterCan = new ImageObject('Pointless Watering Can', 'img/water-can.jpg'));
+imageObjectsArray.push(wineGlass = new ImageObject('Terrible Wine Glass', 'img/wine-glass.jpg'));
 //Data input above...
 
 //Call method to add ID to all image objects below here, using for loop
@@ -54,16 +58,54 @@ function giveAllImageObjectsID(){
 }
 giveAllImageObjectsID();
 
+function putNamesInLabelsArray(){
+  for (var i = 0; i < imageObjectsArray.length; i++){
+    labelsArray.push(imageObjectsArray[i].name);
+  }
+}
+putNamesInLabelsArray();
+
+function putIndividualClicksInArray(){
+  for (var i = 0; i < imageObjectsArray.length; i++){
+    historicalIndividualClicks.push(imageObjectsArray[i].timesClicked);
+  }
+}
+
+function putIndividualDisplaysInArray(){
+  for (var i = 0; i < imageObjectsArray.length; i++){
+    historicalIndividualDisplays.push(imageObjectsArray[i].timesDisplayed);
+  }
+}
+
+function makePercentageArray(){
+  for (var i = 0; i < imageObjectsArray.length; i++){
+    if (imageObjectsArray[i].timesDisplayed === 0){
+      clickPercentageArray.push(0);
+    } else {
+      clickPercentageArray.push((imageObjectsArray[i].timesClicked / imageObjectsArray[i].timesDisplayed) * 100);
+    }
+  }
+}
+
 //set variable for array of 3 images
 possibleChoiceBoxes = document.getElementsByClassName('possibleChoiceBox');
 
 //The function below is for populating the DOM with the images
 function putRandomImagesOnDOM(){
+  var random1 = Math.floor(Math.random() * imageObjectsArray.length);
+  var random2 = random1;
+  while (random2 === random1){
+    random2 = Math.floor(Math.random() * imageObjectsArray.length);
+  }
+  var random3 = random2;
+  while (random3 === random2 || random3 === random1){
+    random3 = Math.floor(Math.random() * imageObjectsArray.length);
+  }
+  var randomArrayOfThree = [random1, random2, random3];
   for (var i = 0; i < possibleChoiceBoxes.length; i++) {
-    var randomizer = Math.floor(Math.random() * imageObjectsArray.length);
-    possibleChoiceBoxes[i].setAttribute('src', imageObjectsArray[randomizer].filePath);
-    possibleChoiceBoxes[i].setAttribute('id', imageObjectsArray[randomizer].myID);
-    imageObjectsArray[randomizer].iWasDisplayed();
+    possibleChoiceBoxes[i].setAttribute('src', imageObjectsArray[randomArrayOfThree[i]].filePath);
+    possibleChoiceBoxes[i].setAttribute('id', imageObjectsArray[randomArrayOfThree[i]].myID);
+    imageObjectsArray[randomArrayOfThree[i]].iWasDisplayed();
   }
 }
 putRandomImagesOnDOM();
@@ -91,7 +133,9 @@ function handleImageClick(event){
 
   putRandomImagesOnDOM();
 
-  if (totalClicks === 5) {
+  if (totalClicks === 25) {
+    putIndividualClicksInArray();
+    putIndividualDisplaysInArray();
     var continueForm = document.createElement('form');
     continueForm.setAttribute('id', 'continueForm');
     var continueFormFieldset = document.createElement('fieldset');
@@ -120,17 +164,19 @@ function handleImageClick(event){
       imageBoxesForStyling[i].style.display = 'none';
     }
   }
-  if (totalClicks === 10){
+  if (totalClicks === 35){
+    putIndividualClicksInArray();
+    putIndividualDisplaysInArray();
     var imageBoxesForStyling = document.getElementsByClassName('possibleChoiceBox');
     for (var i = 0; i < imageBoxesForStyling.length; i++){
       imageBoxesForStyling[i].style.display = 'none';
     }
-    //showData()************************************
+    showData();
   }
 }
 //Click-event handler above
 
-//adding event listener for all 3 images in array created on line `59`
+//adding event listener for all 3 images in array created above putRandomImagesOnDOM definition
 for (var i = 0; i < possibleChoiceBoxes.length; i++) {
   possibleChoiceBoxes[i].addEventListener('click', handleImageClick);
 }
@@ -145,13 +191,13 @@ function handleKeepPlaying(event){
 }
 //...
 function handleSeeData(event){
-  document.getElementById('continueForm').style.display = 'none';
-  //showData***************************************
+  //document.getElementById('continueForm').style.display = 'none';
+  showData();
 }
-
 
 //...
 function showData(){
+  makePercentageArray();
   var mainArea = document.getElementById('mainArea');
   var chartCanvas = document.createElement('canvas');
   chartCanvas.setAttribute('id', 'chartCanvas');
@@ -159,4 +205,38 @@ function showData(){
   chartCanvas.setAttribute('height', '640');
   mainArea.appendChild(chartCanvas);
 
+  //create chart?...
+  var canvasEl = document.getElementById('chartCanvas');
+  var context = canvasEl.getContext('2d');
+
+  var data = {
+    labels: labelsArray,
+    datasets: [
+      {
+        label: 'Clicks',
+        fillColor: '#B8AE9C',
+        strokeColor: '#B8AE9C',
+        highlightFill: '#B8AE9C',
+        highlightStroke: '#B8AE9C',
+        data: historicalIndividualClicks
+      },
+      {
+        label: 'Times Displayed',
+        fillColor: '#ACCFCC',
+        strokeColor: '#ACCFCC',
+        highlightFill: '#ACCFCC',
+        highlightStroke: '#ACCFCC',
+        data: historicalIndividualDisplays
+      },
+      {
+        label: 'Click/Display Percentage',
+        fillColor: '#8A0917',
+        strokeColor: '#8A0917',
+        highlightFill: '#8A0917',
+        highlightStroke: '#8A0917',
+        data: clickPercentageArray
+      }
+    ]
+  };
+  var myChart = new Chart(context).Bar(data);
 }
